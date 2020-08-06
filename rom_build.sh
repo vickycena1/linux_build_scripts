b2b2e7791b99c9781f18832e8e21b141b07898da
Shashank <9945shashank@gmail.com>
2020-08-06 14:48:13 +0000

    Create rom_build.sh

---
+++ rom_build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 PixysOS project.
+# Copyright (C) 2020 Shashank patil custom script.
+#
+# Licensed under the General Public License.
+# This program is free software; you can redistribute it and/or modify
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#
+#
+# ROM building script.
+
+# Colors makes things beautiful
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+# CCACHE UMMM!!! Cooks my builds fast
+
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+export CCACHE_DIR=/home/$username/ccache
+ccache -M 75G
+fi
+
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_EXEC=$(which ccache)
+export CCACHE_DIR=/home/$username/ccache
+ccache -C
+export USE_CCACHE=1
+ccache -M 75G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
+
+# Its Clean Time
+if [ "$make_clean" = "yes" ];
+then
+make clean && make clobber
+wait
+echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
+fi
+
+# Its Images Clean Time
+if [ "$make_clean" = "installclean" ];
+then
+make installclean
+wait
+echo -e ${cya}"Images deleted from OUT dir"${txtrst};
+fi
+
+# Build ROM
+source build/envsetup.sh
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+export TARGET_USES_PREBUILT_CAMERA_SERVICE=false
+export KBUILD_BUILD_HOST="shashank'sBuildBot"
+export KBUILD_BUILD_USER="shashank"
+lunch "$lunch_command"_"$device_codename"-"$build_type"
+make "$target_command" -j"$jobs"
