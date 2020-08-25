951737846854c3c84a10821bd18d75ca13ee64bd
Shashank <9945shashank@gmail.com>
2020-08-25 06:50:32 +0000

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -64,13 +64,11 @@ echo -e ${grn}"compilation started"${txtrst};
 wait
 make -j"$job" O=out \
           ARCH=arm64 \
-          AR=llvm-ar \
           NM=llvm-nm \
           OBJCOPY=llvm-objcopy \
           OBJDUMP=llvm-objdump \
 	  STRIP=llvm-strip \
           CC=clang \
-          HOSTCC=clang mrproper \       
           CLANG_TRIPLE=aarch64-linux-gnu- \
           CROSS_COMPILE=aarch64-linux-gnu- \
 	  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
